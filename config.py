b2812cf0b044f17a3a819e0fa80bba3738dce74b
BharathGottumukkala <34344531+BharathGottumukkala@users.noreply.github.com>
2020-04-14 09:07:08 +0530

    config

---
+++ config.py
@@ -0,0 +1,59 @@
+import json
+
+# def GetAddress(Key):
+# 	with open("config.json", "r") as con:
+# 		try:
+# 			config = json.load(con)
+# 			return config[Key]
+# 		except Exception as e:
+# 			print(e)
+# 			# print("Unable to load config file")
+# 			print("erorr in parsing config file")
+# 			# print("Reinitializing Config file...")
+
+# def UpdateAddress(Key, value):
+# 	with open("config.json", "r") as con:
+# 		config = json.load(con)
+# 		config[Key] = value
+
+# 	with open("config.json", "w") as con:
+# 		json.dump(con, config)
+
+class config(object):
+	"""docstring for config"""
+	def __init__(self):
+		exception = False
+		with open("config.json", "r") as con:
+			try:
+				config = json.load(con)
+			except Exception as e:
+				print("Error in parsing config.")
+				print("Initializing config")
+				exception = True
+		if exception:
+			self.InitializeConfig()
+
+
+	def UpdateAddress(self, key, value):
+		with open("config.json", "r") as con:
+			config = json.load(con)
+		config[key] = value
+		with open("config.json", "w") as con:
+			json.dump(config, con)
+
+	def GetAddress(self, key):
+		with open("config.json", "r") as con:
+			config = json.load(con)
+			return config[key]
+
+	def InitializeConfig(self):
+		with open("config.json", "w") as con:
+			json.dump({}, con)
+
+
+
+if __name__ == '__main__':
+	config = config()
+	config.UpdateAddress('client', '10.1.1.1')
+	print(config.GetAddress('client'))
+
